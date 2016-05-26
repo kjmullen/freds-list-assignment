a5f569ed9e41952eb352facf3a126253f4274949
Kevin Mullen <kevinkozzik@gmail.com>
2016-05-26 13:57:35 -0700

    removed caching, added some more city list styling

--- fredslist/settings.py
+++ fredslist/settings.py
@@ -178,9 +178,9 @@ REST_FRAMEWORK = {
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+#         'LOCATION': '127.0.0.1:11211',
+#     }
+# }
